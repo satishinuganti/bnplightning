6810fc1a332664fb96255fcc18b296834dddc4d1
SatishInuganti <satishinuganti24@gmail.com>
2020-04-26 01:01:01 +0530

    Added new popover changes to account icon in customer 360 v1

--- aura/Account360/Account360Helper.js
+++ aura/Account360/Account360Helper.js
@@ -6,8 +6,18 @@
         });
         action.setCallback(this, function(response){
            var state = response.getState();
-            if(state === 'success'){
-                //console.log(recordId);
+            if(state == 'SUCCESS'){
+                var account = response.getReturnValue();
+                //console.log(account);
+                var accountDetails=[];
+                for(var i=0;i<account.length;i++){
+                    accountDetails.push({
+                        "Name": account[i].Name,
+                        "AccountNumber" :account[i].AccountNumber,
+                        "AccountType": account[i].Type
+                    })
+                }
+                component.set("v.accountRecord",accountDetails);
             }
         });
         $A.enqueueAction(action);
