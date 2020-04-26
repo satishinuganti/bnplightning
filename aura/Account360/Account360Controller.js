a24035fa438331f54544ac232bd903324453fffa
SatishInuganti <satishinuganti24@gmail.com>
2020-04-26 19:00:44 +0530

    New Files added

--- aura/Account360/Account360Controller.js
+++ aura/Account360/Account360Controller.js
@@ -2,5 +2,14 @@
 	fetchAccountDetails : function(component, event, helper) {
         var recordId = component.get("v.recordId");
         helper.fetchAccountRecord(component,event,helper,recordId);
-	}
+        component.set("v.togglehover", true);
+	},
+    fetchPrimaryContact : function(component,event,helper){
+    var recordId = component.get("v.recordId");
+    helper.fetchContactDetails(component,event,helper,recordId);
+        component.set("v.togglehover", true);
+    },
+    reset : function(component, event, helper){
+        component.set("v.togglehover", false);
+    }
 })
--- aura/Account360/Account360Helper.js
+++ aura/Account360/Account360Helper.js
@@ -6,10 +6,45 @@
         });
         action.setCallback(this, function(response){
            var state = response.getState();
-            if(state === 'success'){
-                //console.log(recordId);
+            if(state == 'SUCCESS'){
+                var account = response.getReturnValue();
+                //console.log(account);
+                var accountDetails=[];
+                for(var i=0;i<account.length;i++){
+                    accountDetails.push({
+                        "Name": account[i].Name,
+                        "AccountNumber" :account[i].AccountNumber,
+                        "AccountType": account[i].Type
+                    })
+                }
+                component.set("v.accountRecord",accountDetails);
             }
         });
         $A.enqueueAction(action);
-	}
+	},
+    fetchContactDetails : function(component,event,helper,recordId){
+        var action = component.get("c.getContactDetails");
+        action.setParams({
+            accountId : recordId
+        });
+        action.setCallback(this, function(response){
+           var state = response.getState();
+            if(state == 'SUCCESS'){
+                var contact = response.getReturnValue();
+                var contactDetails=[];
+                for(var i=0;i<contact.length;i++){
+                    contactDetails.push({
+                        "FirstName": contact[i].FirstName,
+                        "LastName" :contact[i].LastName,
+                        "Phone": contact[i].Phone,
+                        "Email": contact[i].Email,
+                        "MobilePhone": contact[i].MobilePhone,
+                        "OtherPhone": contact[i].OtherPhone
+                    })
+                }
+                component.set("v.primaryContact",contactDetails);
+            }
+        });
+        $A.enqueueAction(action);
+    }
 })
