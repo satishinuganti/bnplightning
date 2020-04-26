9b8ec1d64f425fc9b19a358c831fa2b9262e7cb9
SatishInuganti <satishinuganti24@gmail.com>
2020-04-26 19:13:45 +0530

    Files Newer Version v1

--- aura/Account360/Account360Controller.js
+++ aura/Account360/Account360Controller.js
@@ -4,6 +4,11 @@
         helper.fetchAccountRecord(component,event,helper,recordId);
         component.set("v.togglehover", true);
 	},
+    fetchPrimaryContact : function(component,event,helper){
+    var recordId = component.get("v.recordId");
+    helper.fetchContactDetails(component,event,helper,recordId);
+        component.set("v.togglehover", true);
+    },
     reset : function(component, event, helper){
         component.set("v.togglehover", false);
     }
